2ed547325eccae05bfd0acfc6e601aaab693d10d
Robert Waltham <rob@blockoftext.com>
2018-02-19 21:16:40 -0800

    fixed a few more constraints, added class for polyline draw view

---
+++ Stravify/Views/PolyLineView.swift
@@ -0,0 +1,25 @@
+//
+//  PolyLineView.swift
+//  Stravify
+//
+//  Created by Robert Waltham on 2018-02-19.
+//  Copyright © 2018 Robert Waltham. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class PolyLineView: UIView {
+
+    let polyline: GMSPolyline?
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
