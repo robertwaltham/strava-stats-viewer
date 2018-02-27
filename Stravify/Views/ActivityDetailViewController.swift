761280d68f7565b96c174efba0b0487e36fa79d9
Robert Waltham <rob@blockoftext.com>
2018-02-26 18:24:02 -0800

    refactored activity detail to cache streams, added elevation view

--- Stravify/Views/ActivityDetailViewController.swift
+++ Stravify/Views/ActivityDetailViewController.swift
@@ -22,6 +22,7 @@ class ActivityDetailViewController: UIViewController {
         case none = 0
         case heartRate
         case speed
+        case elevation
     }
     
     private let hrColors = [
@@ -45,87 +46,130 @@ class ActivityDetailViewController: UIViewController {
             return
         }
         
-        activity = try? FSInteractor.load(type: Activity.self, id: activityID)
+        let queue: DispatchQueue = ServiceLocator.shared.getService()
         
-        guard let activity = activity else {
-            print("no activity loaded")
-            return
-        }
-        
-        self.navigationItem.title = activity.name
-        
-        // set up camera
-        let path = activity.map.path
-        let bounds = GMSCoordinateBounds(path: path)
-        let update = GMSCameraUpdate.fit(bounds, withPadding: 20)
-        mapView.moveCamera(update)
-        
-        // start marker
-        let start = GMSMarker(position: activity.startLocation)
-        start.title = "Start"
-        start.icon = GMSMarker.markerImage(with: .green)
-        start.map = mapView
-        
-        // end marker
-        let end = GMSMarker(position: activity.endLocation)
-        end.title = "End"
-        end.icon = GMSMarker.markerImage(with: .red)
-        end.map = mapView
-        
-        // draw unstyled line
-        updateMapDisplay(type:.none)
+        queue.async { [unowned self] in
+            self.activity = try? FSInteractor.load(type: Activity.self, id: activityID)
+            
+            guard let activity = self.activity else {
+                print("no activity loaded")
+                return
+            }
+            
+            // Set up bounds and start/finish marker
+            DispatchQueue.main.async { [unowned self] in
+                self.navigationItem.title = activity.name
+                
+                // set up camera
+                let path = activity.map.path
+                let bounds = GMSCoordinateBounds(path: path)
+                let update = GMSCameraUpdate.fit(bounds, withPadding: 20)
+                self.mapView.moveCamera(update)
+                
+                // start marker
+                let start = GMSMarker(position: activity.startLocation)
+                start.title = "Start"
+                start.icon = GMSMarker.markerImage(with: .green)
+                start.map = self.mapView
+                
+                // end marker
+                let end = GMSMarker(position: activity.endLocation)
+                end.title = "End"
+                end.icon = GMSMarker.markerImage(with: .red)
+                end.map = self.mapView
+            }
 
-        // event for control change
-        viewSelector.reactive.controlEvents(.valueChanged).observeValues { [weak self] control in
-            self?.updateMapDisplay(type: ActivityDisplayType(rawValue: control.selectedSegmentIndex) ?? .none)
+            // load high res stream
+            self.loadStream(activity: activity, type: .latlng, resolution: .high) { [unowned self] stream in
+                let path = GMSMutablePath()
+                for point in stream.locationList {
+                    path.add(point)
+                }
+                
+                // create polyline
+                self.polyline = GMSPolyline(path: path)
+                self.polyline?.map = self.mapView
+                
+                // event for control change
+                self.viewSelector.reactive.controlEvents(.valueChanged).observeValues { [weak self] control in
+                    self?.updateMapDisplay(type: ActivityDisplayType(rawValue: control.selectedSegmentIndex) ?? .none)
+                }
+            }
         }
     }
     
+    // Attempts to load stream from disk, else loads from API
+    private func loadStream(activity: Activity, type: StreamType, resolution: StreamResolution, done: @escaping (Stream) -> Void) {
+        let queue: DispatchQueue = ServiceLocator.shared.getService()
+        queue.async {
+            let key = Stream.idForSaving(activity, type, resolution)
+            let cachedStream = try? FSInteractor.load(type: Stream.self, id: key)
+            if let cachedStream = cachedStream {
+                DispatchQueue.main.async {
+                    done(cachedStream)
+                }
+            } else {
+                try? StravaInteractor.getStream(activity: activity, type: type, resolution: .high) { streams in
+                    guard let stream = streams.first(where: { $0.type == type }) else {
+                        print("why is there no \(type.rawValue) stream for \(activity.id)?")
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        done(stream)
+                    }
+                    // save to disk
+                    try? FSInteractor.save(stream, id: key)
+                }
+            }
+        }
+
+    }
+
+    // Updates the polyline styling based on stream type
     private func updateMapDisplay(type: ActivityDisplayType) {
         guard let activity = activity else {
             print("why is there no activity?")
             return
         }
         
-        polyline?.map = nil // clear old polyline
-        
         if type == .none {
-            polyline = GMSPolyline(path: activity.map.path)
-            polyline?.map = mapView
+            polyline?.spans = nil
+            polyline?.strokeWidth = 2
         } else {
             viewSelector.isEnabled = false
             activityIndicator.startAnimating()
             
-            let streamType: StreamType = type == .heartRate ? .heartrate : .velocity_smooth
-            try? StravaInteractor.getStream(activity: activity, type: streamType, resolution: .low) { [weak self] streams in
-                guard let stream = streams.first(where: { $0.type == streamType }) else {
-                    print("why is there no \(streamType.rawValue) stream for \(activity.id)?")
-                    return
+            let streamType: StreamType
+            switch type {
+            case .elevation:
+                streamType = .altitude
+            case .heartRate:
+                streamType = .heartrate
+            case .none:
+                streamType = .distance
+            case .speed:
+                streamType = .moving
+            }
+            loadStream(activity: activity, type: streamType, resolution: .high) { [unowned self] stream in
+                if type == .speed {
+                    self.polyline?.spans = self.computeStyles(stream: stream)
+                } else if type == .heartRate {
+                    let user: StravaUser? = ServiceLocator.shared.tryGetService()
+                    let zones = user?.athlete.computedZones ?? [500]
+                    self.polyline?.spans = self.computeStylesForZones(stream: stream, zones: zones)
+                }  else {
+                    self.polyline?.spans = self.computeStyles(stream: stream)
                 }
+                self.polyline?.strokeWidth = 5
                 
-                DispatchQueue.main.async {
-                    let polyline = GMSPolyline(path: activity.map.path)
-                    
-                    if type == .speed {
-                        polyline.spans = self?.computeStyles(stream: stream)
-                    } else {
-                        let user: StravaUser? = ServiceLocator.shared.tryGetService()
-                        let zones = user?.athlete.computedZones ?? [500]
-                        polyline.spans = self?.computeStylesForZones(stream: stream, zones: zones)
-                    }
-                    polyline.map = self?.mapView
-                    polyline.strokeWidth = 5
-                    self?.polyline = polyline
-                    
-                    self?.viewSelector.isEnabled = true
-                    self?.activityIndicator.stopAnimating()
-                }
+                self.viewSelector.isEnabled = true
+                self.activityIndicator.stopAnimating()
             }
         }
     }
     
+    // compute polyline styles for heart rate zones
     private func computeStylesForZones(stream: Stream, zones: [Int]) -> [GMSStyleSpan] {
-        print(zones)
         return stream.data.map { value in
             var color = hrColors.first
             for (i, zone) in zones.enumerated() {
@@ -138,6 +182,7 @@ class ActivityDetailViewController: UIViewController {
         }
     }
     
+    // compoute polyline styles for relative speed and elevation
     private func computeStyles(stream: Stream) -> [GMSStyleSpan] {
         
         var max: Double = 0
@@ -152,8 +197,6 @@ class ActivityDetailViewController: UIViewController {
                 min = value
             }
         }
-        
-        print("max: \(max) min: \(min)")
         
         return stream.data.map { value in
             let color = UIColor(hue: 0.3, saturation: CGFloat((value - min) / max), brightness: 1, alpha: 1)
